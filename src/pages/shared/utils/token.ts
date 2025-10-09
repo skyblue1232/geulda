3826d1d0f1baa8a9ec3d77fb031e0af3cb005fc2
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-10-10 02:13:06 +0900

    deploy/test

--- src/pages/shared/utils/token.ts
+++
@@ -1,3 +0,0 @@
-// 로컬스토리지에서 토큰을 get & set
-export const getAccessToken = () => localStorage.getItem('accessToken');
-export const setAccessToken = (token: string) => localStorage.setItem('accessToken', token);
