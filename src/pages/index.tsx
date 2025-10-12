c30c74fbb54926e76883169deedbbe98c362da06
sumin <sumin50240585@gmail.com>
2025-10-12 18:20:35 +0900

    setting : 컬러 & 폰트 디자인 시스템 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,58 @@
-export default function Home() {
+import React from 'react';
+
+export default function TypographyDemo() {
   return (
-    <>
-      <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50'>
-        <main className='flex flex-col items-center justify-center w-full flex-1 px-4 sm:px-20 text-center'>
-          <h1 className='text-4xl sm:text-6xl font-extrabold text-gray-900 mb-4'>
-            초기 세팅 완료
-          </h1>
-          <p className='mt-3 text-lg sm:text-2xl text-gray-600'>
-            Next.js(Page Router) + TS + Tailwind + Axios
-          </p>
-        </main>
-      </div>
-    </>
+    <div className='p-8 space-y-12 bg-background text-foreground min-h-screen'>
+      {/* Pretendard Display */}
+      <section>
+        <h2 className='bg-main-1 text-2xl font-bold mb-4'>
+          Pretendard Display
+        </h2>
+        {/* 이렇게 사용 Tailwind가 자동으로 반응형 처리 */}
+        <p className="text-display-lg md:text-display-md lg:text-display-sm">Display Large - Pretendard</p>
+        <p className='text-main-10 text-display-md'>
+          Display Medium - Pretendard
+        </p>
+        <p className='text-display-sm'>Display Small - Pretendard</p>
+      </section>
+
+      {/* Pretendard Headline */}
+      <section>
+        <h2 className=' text-2xl font-bold mb-4'>Pretendard Headline</h2>
+        <p className='text-secondary-8 text-headline-lg'>
+          Headline Large - Pretendard
+        </p>
+        <p className='text-headline-md'>Headline Medium - Pretendard</p>
+        <p className='text-headline-sm'>Headline Small - Pretendard</p>
+      </section>
+
+      {/* Pretendard Title / Label / Body */}
+      <section>
+        <h2 className='text-gray-8 text-2xl font-bold mb-4'>
+          Pretendard Title / Label / Body
+        </h2>
+        <p className='text-title-lg'>Title Large</p>
+        <p className='text-title-md'>Title Medium</p>
+        <p className='text-title-sm'>Title Small</p>
+
+        <p className='text-label-lg'>Label Large</p>
+        <p className='text-label-md'>Label Medium</p>
+        <p className='text-label-sm'>Label Small</p>
+
+        <p className='text-body-lg'>Body Large</p>
+        <p className='text-body-md'>Body Medium</p>
+        <p className='text-body-sm'>Body Small</p>
+      </section>
+
+      {/* JEN Serif Display / Headline / Label */}
+      <section>
+        <h2 className='text-2xl font-bold mb-4'>JEN Serif</h2>
+        <p className='text-display-serif'>Display - JEN Serif</p>
+        <p className='text-headline-lg-serif'>Headline Large - JEN Serif</p>
+        <p className='text-headline-md-serif'>Headline Medium - JEN Serif</p>
+        <p className='text-headline-sm-serif'>Headline Small - JEN Serif</p>
+        <p className='text-label-serif'>Label - JEN Serif</p>
+      </section>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,209 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          1: 'var(--color-main-1)',
+          2: 'var(--color-main-2)',
+          3: 'var(--color-main-3)',
+          4: 'var(--color-main-4)',
+          5: 'var(--color-main-5)',
+          6: 'var(--color-main-6)',
+          7: 'var(--color-main-7)',
+          8: 'var(--color-main-8)',
+          9: 'var(--color-main-9)',
+          10: 'var(--color-main-10)',
+        },
+        secondary: {
+          1: 'var(--color-secondary-1)',
+          2: 'var(--color-secondary-2)',
+          3: 'var(--color-secondary-3)',
+          4: 'var(--color-secondary-4)',
+          5: 'var(--color-secondary-5)',
+          6: 'var(--color-secondary-6)',
+          7: 'var(--color-secondary-7)',
+          8: 'var(--color-secondary-8)',
+          9: 'var(--color-secondary-9)',
+          10: 'var(--color-secondary-10)',
+        },
+        gray: {
+          1: 'var(--color-gray-1)',
+          2: 'var(--color-gray-2)',
+          3: 'var(--color-gray-3)',
+          4: 'var(--color-gray-4)',
+          5: 'var(--color-gray-5)',
+          6: 'var(--color-gray-6)',
+          7: 'var(--color-gray-7)',
+          8: 'var(--color-gray-8)',
+          9: 'var(--color-gray-9)',
+          10: 'var(--color-gray-10)',
+        },
+        blue: 'var(--color-blue)',
+        red: {
+          1: 'var(--color-red-1)',
+          2: 'var(--color-red-2)',
+        },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'system-ui', 'sans-serif'],
+        serif: ['JEN Serif', 'serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        /* Pretendard */
+        /* DISPLAY */
+        '.text-display-lg': {
+          fontSize: '3.5625rem', //57px
+          fontWeight: 300,
+          lineHeight: '4rem', //64px
+          letterSpacing: '-0.015625rem', //-0.25px',
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-display-md': {
+          fontSize: '3.25rem', //52px
+          fontWeight: 300,
+          lineHeight: '2.8125rem', //45px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-display-sm': {
+          fontSize: '2.25rem', //36px
+          fontWeight: 300,
+          lineHeight: '2.75rem', //44px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        /* HEADLINE */
+        '.text-headline-lg': {
+          fontSize: '2rem', //32px
+          fontWeight: 200,
+          lineHeight: '2.5rem', //40px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-headline-md': {
+          fontSize: '1.75rem', //28px
+          fontWeight: 200,
+          lineHeight: '2rem', //32px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-headline-sm': {
+          fontSize: '1.5rem', //24px
+          fontWeight: 200,
+          lineHeight: '2rem', //32px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+
+        /* TITLE */
+        '.text-title-lg': {
+          fontSize: '1.375rem', //22px
+          fontWeight: 400,
+          lineHeight: '1.75rem', //28px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-title-md': {
+          fontSize: '1rem', //16px
+          fontWeight: 400,
+          lineHeight: '1.5rem', //24px
+          letterSpacing: '0.009375rem', //0.15px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-title-sm': {
+          fontSize: '0.875rem', //14px
+          fontWeight: 400,
+          lineHeight: '1.25rem', //20px
+          letterSpacing: '0.00625rem', //0.1px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        /* LABEL */
+        '.text-label-lg': {
+          fontSize: '0.875rem', //14px
+          fontWeight: 300,
+          lineHeight: '1.25rem', //20px
+          letterSpacing: '0.00625rem', //0.1px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-label-md': {
+          fontSize: '0.75rem', //12px
+          fontWeight: 300,
+          lineHeight: '1rem', //16px
+          letterSpacing: '0.03125rem', //0.5px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-label-sm': {
+          fontSize: '0.6875rem', //11px
+          fontWeight: 300,
+          lineHeight: '1rem', //16px
+          letterSpacing: '0.03125rem', //0.5px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        /* BODY */
+        '.text-body-lg': {
+          fontSize: '0.875rem', //14px,
+          fontWeight: 400,
+          lineHeight: '1.5rem', //24px
+          letterSpacing: '0.03125rem', //0.5px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-body-md': {
+          fontSize: '0.75rem', //12px
+          fontWeight: 300,
+          lineHeight: '1.25rem', //20px
+          letterSpacing: '0.015625rem', //0.25px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+        '.text-body-sm': {
+          fontSize: '0.625rem', //10px
+          fontWeight: 200,
+          lineHeight: '1rem', //16px
+          letterSpacing: '0.025rem', // 0.4px
+          fontFamily: 'Pretendard, system-ui, sans-serif',
+        },
+
+        /* JEN Serif */
+        /* DISPLAY */
+        '.text-display-serif': {
+          fontSize: '2.25rem', //36px
+          fontWeight: 200,
+          lineHeight: '2.875rem', //46px
+          letterSpacing: '0.375rem', //6px
+          fontFamily: 'JEN Serif, serif',
+        },
+        /* HEADLINE */
+        '.text-headline-lg-serif': {
+          fontSize: '2rem', //32px
+          fontWeight: 200,
+          lineHeight: '2.5rem', //40px
+          letterSpacing: '0.125rem', //2px
+          fontFamily: 'JEN Serif, serif',
+        },
+        '.text-headline-md-serif': {
+          fontSize: '1.5rem', //24px
+          fontWeight: 200,
+          lineHeight: '2rem', //32px
+          letterSpacing: '0.125rem', //2px
+          fontFamily: 'JEN Serif, serif',
+        },
+        '.text-headline-sm-serif': {
+          fontSize: '1.25rem', //20px
+          fontWeight: 200,
+          lineHeight: '1.5rem', //24px
+          letterSpacing: '0.75rem', //12px
+          fontFamily: 'JEN Serif, serif',
+        },
+        /* LABEL */
+        '.text-label-serif': {
+          fontSize: '0.875rem', //14px
+          fontWeight: 200,
+          lineHeight: '1.25rem', //20px
+          letterSpacing: '0.25rem', //4px
+          fontFamily: 'JEN Serif, serif',
+        },
+      };
+      addUtilities(newUtilities, { variants: ['responsive'] });
+    },
+  ],
 };
