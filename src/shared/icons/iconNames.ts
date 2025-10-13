ba1402cec5f209ac20e764acb26e9a6fd3533245
jjangminii <wjdals030328@catholic.ac.kr>
2025-10-14 01:24:39 +0900

    feat: 이미지 테스트

--- src/shared/icons/iconNames.ts
+++ src/shared/icons/iconNames.ts
@@ -1,3 +1,3 @@
 // 이 파일은 자동 생성 파일입니다. (직접 수정 금지)
-export const iconNames = [] as const;
-export type IconName = typeof iconNames[number];
+export const iconNames = ['CalendarBlank', 'User'] as const;
+export type IconName = (typeof iconNames)[number];
