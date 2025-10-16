bc4a83dcd040f4b1ab1db4c01da8e2a7c2f5a1d1
sumin <sumin50240585@gmail.com>
2025-10-16 15:57:24 +0900

    fix : Tailwind 적용 오류 및 토큰 재설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,8 @@
-export default function Home() {
+export default function HomePage() {
   return (
-    <>
-      <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50'>
-        <main className='flex flex-col items-center justify-center w-full flex-1 px-4 sm:px-20 text-center'>
-          <h1 className='text-4xl sm:text-6xl font-extrabold text-gray-900 mb-4'>
-            초기 세팅 완료
-          </h1>
-          <p className='mt-3 text-lg sm:text-2xl text-gray-600'>
-            Next.js(Page Router) + TS + Tailwind + Axios
-          </p>
-        </main>
-      </div>
-    </>
+    <div className='bg-mint-500 text-background p-8 '>
+      <h1 className='p-20 text-headline-lg-serif'>테스트</h1>
+      <p className='text-pink-400 text-display-lg'>되거라</p>
+    </div>
   );
 }
