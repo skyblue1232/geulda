5c6f604b377725f1c3acfe67eeca6c56f934cd9f
jjangminii <wjdals030328@catholic.ac.kr>
2025-10-17 18:06:10 +0900

    chore: 안쓰는 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,6 @@ export default function Home() {
     <>
       <div className='flex flex-col items-center justify-center  min-h-screen py-2 bg-gray-50'>
         <main className='flex flex-col items-center justify-center w-full flex-1 px-4 sm:px-20 text-center'>
-          <Header onClick={() => {}} className='' title={'글다'} />
-          <ControlBar isLoggedIn={false} onLogin={() => {}} />
-          <ControlBar isLoggedIn={true} userName='홍길동' />
           <h1 className='text-4xl sm:text-6xl font-extrabold text-gray-900 mb-4'>
             초기 세팅 완료
             <Icon name='User' color='mint-600' />
