2c984b4360c84ae56b90dbcfe361e1fcece05911
sumin <sumin50240585@gmail.com>
2025-10-18 22:21:47 +0900

    feat : 장소 말풍선 사용 컴포넌트

---
+++ src/shared/components/set/LocationBubbleSet.tsx
@@ -0,0 +1,40 @@
+import Overlay from '@/shared/common/Overlay';
+import LocationBubble from '@/shared/components/container/LocationBubble';
+import { cn } from '@/shared/lib';
+
+interface LocationBubbleSetProps {
+  isVisible: boolean;
+  onClose?: () => void;
+  name: string;
+  imageSrc?: string;
+  className?: string;
+}
+
+const LocationBubbleSet = ({
+  isVisible,
+  onClose,
+  name,
+  imageSrc,
+  className,
+}: LocationBubbleSetProps) => {
+  if (!isVisible) return null;
+
+return (
+    <>
+      <Overlay onClick={onClose} />
+      <div
+        className={cn(
+          'fixed inset-0 z-[50] flex justify-center items-center pointer-events-none',
+          className,
+        )}
+      >
+        <div className="pointer-events-auto">
+          <LocationBubble name={name} imageSrc={imageSrc} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+export default LocationBubbleSet;
