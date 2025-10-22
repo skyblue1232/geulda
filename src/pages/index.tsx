204b3e8f55871b432f9e429b8e87d907631ab311
jjangminii <wjdals030328@catholic.ac.kr>
2025-10-23 02:58:38 +0900

    chore: 주석 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,6 @@ export default function Home() {
             초기 세팅 완료
             <Icon name='User' color='mint-600' />
           </h1>
-          <StampBoard count={3} total={8} />
-          <StampBoard count={3} total={9} />
-          <StampBoard count={3} total={10} />
           <p className='mt-3 text-lg sm:text-2xl text-gray-600'>
             Next.js(Page Router) + TS + Tailwind + Axios
           </p>
