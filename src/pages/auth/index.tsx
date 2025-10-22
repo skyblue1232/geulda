28b4187d02a33a3dfd4c77d9c000d620d1513634
sumin <sumin50240585@gmail.com>
2025-10-23 03:14:44 +0900

    feat : 로그인 페이지 - 패딩, 갭 값 미세 조정 필요

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,21 +1,79 @@
-/* 로그인 페이지 */
 'use client';
-
+import { Icon } from '@/shared/icons';
 import KakaoLoginButton from './components/KakaoLoginButton';
 import GoogleLoginButton from './components/GoogleLoginButton';
 import RecentLoginBubble from './components/RecentLoginBubble';
 
 export default function LoginPage() {
-  const handleClick = () => {
-    alert('카카오 버튼 클릭!');
-  };
+  const handleClick = () => alert('로그인 버튼 클릭');
 
   return (
-    <main className='flex flex-col items-center justify-center min-h-screen bg-gray-50 gap-6'>
-      {/* 버튼 렌더링 */}
-      <KakaoLoginButton onClick={handleClick} />
-      <GoogleLoginButton onClick={handleClick} />
-      <RecentLoginBubble />
+    <main className='w-full flex flex-col items-center overflow-hidden bg-white'>
+      {/* 그라데이션 영역 */}
+      <div className='w-full'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 402 194'
+          className='w-full h-auto'
+        >
+          <path
+            d='M402 180.922C257.925 56.9753 160.195 270.883 0 161.93V0H402V180.922Z'
+            fill='url(#paint0_linear_mint)'
+          />
+          <defs>
+            <linearGradient
+              id='paint0_linear_mint'
+              x1='201.53'
+              y1='0'
+              x2='201'
+              y2='163'
+              gradientUnits='userSpaceOnUse'
+            >
+              <stop stopColor='#D9FAFB' />
+              <stop offset='1' stopColor='#8CEFF2' />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+
+      {/* 콘텐츠  */}
+      <section className='w-full flex flex-col items-center text-center px-[2.4rem] pt-[3.2rem] pb-[6rem]'>
+        {/* 타이틀 */}
+        <div className='flex flex-col items-center gap-[1.2rem] mb-[4rem]'>
+          <h1 className='text-headline-lg-serif text-mint-900'>글다</h1>
+          <p className='text-label-md text-mint-900 leading-[1.8rem]'>
+            만화 속 부천 여행
+            <br />
+            8개 명소를 탐험하고 엽서를 모아보세요!
+          </p>
+        </div>
+
+        {/* 로고 */}
+        <div className='mb-[6.8rem]'>
+          <Icon name='Stamp' size={196} color='mint-400' />
+        </div>
+
+        {/* 로그인 버튼 */}
+        <div className='flex flex-col items-center gap-[3.2rem]'>
+          <div className='flex flex-col items-center gap-[0.8rem]'>
+            <p className='text-label-lg text-gray-400'>start with</p>
+          </div>
+
+          <div className='flex gap-[2.4rem]'>
+            <KakaoLoginButton onClick={handleClick} />
+            <GoogleLoginButton onClick={handleClick} />
+          </div>
+
+          <p className='text-label-md text-gray-400 undezrline cursor-pointer'>
+            비회원 로그인
+          </p>
+        </div>
+
+        {/* 안내문 */}
+        <p className='mt-[5rem] text-label-sm text-gray-400'>
+          비회원은 스탬프 저장과 공유 기능을 사용할 수 없습니다.
+        </p>
+      </section>
     </main>
   );
 }
