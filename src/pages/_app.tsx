c7361369aff658ce57dba36860f8d18861bb504f
sumin <sumin50240585@gmail.com>
2025-10-23 12:58:00 +0900

    refact : 코드래빗 리뷰 반영

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import '@/styles/globals.css';
 import '@/shared/icons';
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
