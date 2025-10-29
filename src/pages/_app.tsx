50f6cdd2d5ce46fc85c9ca3da69fb38796fb275a
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-10-29 21:41:39 +0900

    Revert "🌐Deploy: 배포 버전1"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,6 @@
-import '@/styles/globals.css';
-import '@/shared/icons';
-import { useState } from 'react';
-import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-          },
-        },
-      }),
-  );
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/shared/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './header/Header';
-export { default as ControlBar } from './header/ControlBar';
-export { default as Tag } from './tag/Tag';
-export { DatePicker } from './datePicker/DatePicker';
