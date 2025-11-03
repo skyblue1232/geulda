2759960ed0f10c684365b7715d8498c0f5bf818c
sumin <sumin50240585@gmail.com>
2025-11-04 01:32:08 +0900

    fix : build error 해결

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -38,7 +38,7 @@ let failedQueue: {
   reject: (error: unknown) => void;
 }[] = [];
 
-const processQueue = (error: any, token: string | null = null) => {
+const processQueue = (error: unknown, token: string | null = null) => {
   failedQueue.forEach((prom) => {
     if (error) prom.reject(error);
     else prom.resolve(token ?? '');
