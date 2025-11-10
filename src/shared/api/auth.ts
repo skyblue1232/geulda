a2f8339cdea8808a22beb706108469c5e77d3ac2
sumin <sumin50240585@gmail.com>
2025-11-10 22:14:14 +0900

    feat : 로그인 api

---
+++ src/shared/api/auth.ts
@@ -0,0 +1,17 @@
+import { apiAuth } from '@/shared/api/instance';
+import type { ApiResponse, TokenData } from '@/shared/types/authtypes';
+import { setTokens } from '@/shared/utils/token';
+
+export const exchangeTempToken = async (tempToken: string) => {
+  const { data } = await apiAuth.post<ApiResponse<TokenData>>(
+    '/api/auth/temp-token/exchange',
+    { tempToken },
+  );
+
+  if (data.success) {
+    const { accessToken, refreshToken } = data.data;
+    setTokens(accessToken, refreshToken);
+  }
+
+  return data;
+};
