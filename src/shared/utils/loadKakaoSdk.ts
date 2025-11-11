6296b1ae8785d768b7c5bae344c49c56ca298a25
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-11-12 03:09:03 +0900

    chore/map-data

--- src/shared/utils/loadKakaoSdk.ts
+++ src/shared/utils/loadKakaoSdk.ts
@@ -9,7 +9,7 @@ export function loadKakaoSdk(callback: () => void) {
 
   const script = document.createElement('script');
   script.id = scriptId;
-  script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
+  script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&autoload=false`;
   script.async = true;
   script.onload = callback;
   document.head.appendChild(script);
