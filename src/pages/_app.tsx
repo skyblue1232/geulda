68f5a2839d30342dba7d123f5ef4156914a469d3
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-11-16 19:32:27 +0900

    settng/next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import '@/shared/icons';
 import { useState } from 'react';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppLayout from '@/shared/layout/AppLayout';
@@ -18,10 +19,25 @@ export default function App({ Component, pageProps }: AppProps) {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </QueryClientProvider>
+    <>
+      <Head>
+        {/* PWA 설정 */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        {/* iOS 전용 옵션 */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Geulda" />
+        <link rel="apple-touch-icon" href="/icons/icon3.png" />
+      </Head>
+
+      <QueryClientProvider client={queryClient}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </QueryClientProvider>
+    </>
   );
 }
